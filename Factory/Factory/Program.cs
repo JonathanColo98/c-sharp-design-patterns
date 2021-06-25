0cccac7a9563571479fafe3b476c2046178801da
JonathanColo98 <colombojonathan@virgilio.it>
2021-06-25 12:48:54 +0200

    Factory done

---
+++ Factory/Factory/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Factory
+{
+    public abstract class Creator
+    {
+        public abstract IProduct FactoryMethod();
+
+        public string SomeOperation()
+        {
+            var product = FactoryMethod();
+            var result = "Creator: The same creator's code has just worked with "
+            + product.Operation();
+
+            return result;
+        }
+    }
+
+    public class ConcreteCreatorA : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    public class ConcreteCreatorB : Creator
+    {
+        public override IProduct FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+
+    public interface IProduct
+    {
+        public string Operation();
+    }
+
+    public class ConcreteProductA : IProduct
+    {
+        public string Operation()
+        {
+            return "Concrete Product A";
+        }
+    }
+
+    public class ConcreteProductB : IProduct
+    {
+        public string Operation()
+        {
+            return "Concrete Product B";
+        }
+    }
+
+    class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("App: Launched with the ConcreteCreator1.");
+            ClientCode(new ConcreteCreatorA());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("App: Launched with the ConcreteCreator2.");
+            ClientCode(new ConcreteCreatorB());
+        }
+
+        public void ClientCode(Creator creator)
+        {
+            // ...
+            Console.WriteLine("Client: I'm not aware of the creator's class," +
+                "but it still works.\n" + creator.SomeOperation());
+            // ...
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Client().Main();
+        }
+    }
+}
