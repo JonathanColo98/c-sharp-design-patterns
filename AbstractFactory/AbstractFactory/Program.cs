117ac0e68cc0340b12c10f23fde11bddac035a66
JonathanColo98 <colombojonathan@virgilio.it>
2021-06-28 18:01:10 +0200

    Abstract factory not finished

---
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+
+    public interface IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA();
+
+        public IAbstractProductB CreateProductB();
+    }
+
+    public class ConcreteFactory1 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA1();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB1();
+        }
+    }
+
+    public class ConcreteProductA1 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A1.";
+        }
+    }
+
+    public class ConcreteProductB1 : IAbstractProductB
+    {
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UsefulFunctionB()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ConcreteFactory2 : IAbstractFactory
+    {
+        public IAbstractProductA CreateProductA()
+        {
+            return new ConcreteProductA2();
+        }
+
+        public IAbstractProductB CreateProductB()
+        {
+            return new ConcreteProductB2();
+        }
+    }
+
+    public interface IAbstractProductA
+    {
+        public string UsefulFunctionA();
+    }
+
+    public interface IAbstractProductB
+    {
+        public string UsefulFunctionB();
+
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
