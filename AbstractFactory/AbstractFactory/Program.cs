395bf2de4df5439b49f5d192121c256c1333be1e
JonathanColo98 <colombojonathan@virgilio.it>
2021-08-04 10:52:45 +0200

    Finished Abstract Factory

--- AbstractFactory/AbstractFactory/Program.cs
+++ AbstractFactory/AbstractFactory/Program.cs
@@ -2,7 +2,6 @@
 
 namespace AbstractFactory
 {
-
     public interface IAbstractFactory
     {
         public IAbstractProductA CreateProductA();
@@ -20,27 +19,6 @@ namespace AbstractFactory
         public IAbstractProductB CreateProductB()
         {
             return new ConcreteProductB1();
-        }
-    }
-
-    public class ConcreteProductA1 : IAbstractProductA
-    {
-        public string UsefulFunctionA()
-        {
-            return "The result of the product A1.";
-        }
-    }
-
-    public class ConcreteProductB1 : IAbstractProductB
-    {
-        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string UsefulFunctionB()
-        {
-            throw new NotImplementedException();
         }
     }
 
@@ -62,6 +40,22 @@ namespace AbstractFactory
         public string UsefulFunctionA();
     }
 
+    public class ConcreteProductA1 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A1.";
+        }
+    }
+
+    public class ConcreteProductA2 : IAbstractProductA
+    {
+        public string UsefulFunctionA()
+        {
+            return "The result of the product A2.";
+        }
+    }
+
     public interface IAbstractProductB
     {
         public string UsefulFunctionB();
@@ -69,11 +63,63 @@ namespace AbstractFactory
         public string AnotherUsefulFunctionB(IAbstractProductA collaborator);
     }
 
+    public class ConcreteProductB1 : IAbstractProductB
+    {
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B1 collaborating with the ({result})";
+        }
+
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B1.";
+        }
+    }
+
+    public class ConcreteProductB2 : IAbstractProductB
+    {
+        public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
+        {
+            var result = collaborator.UsefulFunctionA();
+
+            return $"The result of the B2 collaborating with the ({result})";
+        }
+
+        public string UsefulFunctionB()
+        {
+            return "The result of the product B2.";
+        }
+    }
+
+    public class Client
+    {
+        public void Main()
+        {
+            Console.WriteLine("Client: Testing client code with the first factory type...");
+            ClientMethod(new ConcreteFactory1());
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Testing client code with the second factory type...");
+            ClientMethod(new ConcreteFactory2());
+        }
+
+        public void ClientMethod(IAbstractFactory factory)
+        {
+            var productA = factory.CreateProductA();
+            var productB = factory.CreateProductB();
+
+            Console.WriteLine(productB.UsefulFunctionB());
+            Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Client().Main();
         }
     }
 }
